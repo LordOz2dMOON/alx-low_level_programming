4203b81ebeb975ea4473215fe6b26581266c1a0d
Oswald <oswaldojo@ymail.com>
2022-07-01 16:53:32 -0700

    correct

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- * /
+ * Return: Always 0 (Success) /
+ */
 int main(void)
 {
 	int i;
 
-	for(i = 0; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		printf("%d\n", i);
 	}
