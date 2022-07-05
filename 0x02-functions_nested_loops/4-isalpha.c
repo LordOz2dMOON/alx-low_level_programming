7f6bd4678d9aeee92d8542936a9f4cf643616ce9
Oswald <oswaldojo@ymail.com>
2022-07-05 16:49:02 -0700

    isalpha check

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,8 +10,12 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && 'a') || (c <= 'Z' && 'z'))
+	if (c >= 'A' && c <= 'Z')
 	return (1);
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
 	else
 	{
 		return (0);
