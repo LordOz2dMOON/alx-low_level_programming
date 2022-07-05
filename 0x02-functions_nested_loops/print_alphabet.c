e0b3becf667011ce22e6b41603bfc5937151a08b
Oswald <oswaldojo@ymail.com>
2022-07-05 14:42:38 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Contains a function that prints small letters of the alphabet
+ *
+ * Return: Always 0: (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char l;
+
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar('l');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
