261c6c986e1756e8b88d9d594fa7d48a6b3e9617
Oswald <oswaldojo@ymail.com>
2022-07-05 15:21:45 -0700

    10times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - prints 10 times lowercase alphabet
+ *
+ * Return - Always 0: (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char l;
+
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			putchar(l)
+		}
+		putchar('\n')
+	}
+}
