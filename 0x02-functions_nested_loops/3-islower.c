0af9641500071cb180a77fa54d5731fa9f0ae05c
Oswald <oswaldojo@ymail.com>
2022-07-05 15:52:41 -0700

    is lower check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * islower - check if the alphabet is lowercase
+ * _islower - check if the alphabet is lowercase
  *
  * Return - Always 0: (Success)
  */
-int _islower(int c)
+int _islower(int c/*collect an integer c and check it*/)
 {
 	if (c >= 'a' && c <= 'z')
-	return 1;
+	/*return 1 if its a lowercase letter*/
+	return (1);
 	else
-	{
-		return 0;
+	{	/*return 0 if its not lowercase*/
+		return (0);
 	}
 }
