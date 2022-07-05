b62e9b9a4248ce7cf15e38163a92352e7dd38715
Oswald <oswaldojo@ymail.com>
2022-07-05 15:43:46 -0700

    is lower check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * islower - check if the alphabet is lowercase
+ *
+ * Return - Always 0: (Success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return 1;
+	else
+	{
+		return 0;
+	}
+}
