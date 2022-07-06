3488c9523a1ae71544b23437a4ff4d270d847885
Oswald <oswaldojo@ymail.com>
2022-07-05 20:23:15 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - print every minute of the day
+ * Return: void
  */
 void jack_bauer(void)
 {
-	
+	int m, h;
+
+	for (m = 0; m <= 23; m++)
+	{
+		for (h = 0; h <= 59; h++)
+		{
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar(':');
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
