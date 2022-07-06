20fffb880b421610acbb3bf23f7a74c0c08adbf3
Oswald <oswaldojo@ymail.com>
2022-07-05 17:02:59 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * sign- print the sign of a number
+ *
+ * @n: Collects an integer n
+ *
+ * Return - Always 0: (Success)
+ */
+int print_sign(int n)
+{
+
+	if(n > 0)
+	{
+		_putchar('+')
+		_putchar(',')
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+}
