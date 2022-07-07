a2974a3e59e097a14639fe7c7ca4325d9d11d4d8
Oswald <oswaldojo@ymail.com>
2022-07-07 10:32:14 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0-9
+ */
+void print_numbers(void)
+{	int n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_puthcar(i);
+		_putchar('\n');
+	}
+}
