25752721b5fa2d3332383f36a6431824a7ed280e
Oswald <oswaldojo@ymail.com>
2022-07-07 09:20:05 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,12 @@
 /**
  * _isupper - checks for uppercase character
  * @c: holds the character
- * Return: on Success 1
+ * Return: on Success 1 and on failure 0
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-	return (1);
+		return (1);
 	else
-	{
 		return (0);
-	}
 }
