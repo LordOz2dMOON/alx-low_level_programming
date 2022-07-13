09feb9b6efdd6b0961b091e7b1e3dcc974f03bd9
Oswald Ojo <oswaldojo@ymail.com>
2022-07-13 01:40:03 +0000

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - function that prints every other character of a string,
@@ -8,9 +9,12 @@
  */
 void puts2(char *str)
 {
-	for (; *str <= '\0'; str += 2)
+	int len, i;
+
+	len = strlen(str)
+	for (i = 0; i < len; i += 2)
 	{
-		_putchar(str);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
