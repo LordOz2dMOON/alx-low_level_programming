2cf72129f16fc68663984d79bdd759ae45f96f25
Oswald Ojo <oswaldojo@ymail.com>
2022-07-15 01:54:40 +0000

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercase
+ * @str: a string
+ * Return: a character
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+}
