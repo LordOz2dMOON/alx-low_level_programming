463e151505ba7e29f27b591243e4672fc66f054b
Oswald Ojo <oswaldojo@ymail.com>
2022-07-15 01:56:53 +0000

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,9 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-		}
+	if (str[i] <= 'z' && str[i] >= 'a')
+	str[i] -= 32;
 	}
+
+	return (str);
 }
