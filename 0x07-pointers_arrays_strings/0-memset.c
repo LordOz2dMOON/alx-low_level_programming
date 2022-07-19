d28ed18e8c75473a2b8660fa7d6dbcd226a4fda3
Oswald Ojo <oswaldojo@ymail.com>
2022-07-19 23:19:19 +0000

    even more arrays

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to an array
+ * @b: character
+ * @n: unsigned int
+ * Return: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
