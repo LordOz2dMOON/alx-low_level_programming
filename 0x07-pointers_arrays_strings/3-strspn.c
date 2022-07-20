db84f837febbc7c4e66f8f3f7fc1b2b01f500573
Oswald Ojo <oswaldojo@ymail.com>
2022-07-20 02:56:09 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, count;
 
-	while (i)
+	i = 0;
+	count = 0;
+
+	while (1)
 	{
 		while (accept[i] == s[i])
 		{
 			count += 1;
 			i++;
+			if (accept[i] != s[i])
+			{
+				break;
+			}
 		}
-		if (accept[i] != s[i])
-			break;
-	}
 	return (count);
 }
