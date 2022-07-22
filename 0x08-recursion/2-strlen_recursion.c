1d906104852510ab7218c939a72b261858537426
Oswald Ojo <oswaldojo@ymail.com>
2022-07-22 02:55:59 +0000

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to a string
+ * Return: Integer
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == '\0')
+		return (count);
+	s++;
+	_strlen_recursion(s)
+	count += 1;
+}
