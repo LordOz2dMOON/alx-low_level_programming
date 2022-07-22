7fe8507355a702b9caf2207d792659bea871b70f
Oswald Ojo <oswaldojo@ymail.com>
2022-07-22 03:17:29 +0000

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,17 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s == '\0')
-	{
-		return (count);
-	}
-	else
-	{
-		count++;
-		s++;
-		_strlen_recursion(s);
-	}
-	return (0);
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
