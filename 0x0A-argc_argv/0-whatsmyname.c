b671059b80e78cfe3a124bd85149c56139ed4fe5
Oswald Ojo <oswaldojo@ymail.com>
2022-07-28 01:04:44 +0000

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#include <stdio.h>
+
 
 /**
  * main - prints it's name followed by a new line
@@ -7,7 +8,7 @@
  * Return: string
  */
 
-int main(argc, char *argv[])
+int main(argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
