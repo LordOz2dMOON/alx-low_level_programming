cadefb0b10e425091fc251fbb1e2599491f023ea
Oswald Ojo <oswaldojo@ymail.com>
2022-07-28 00:50:28 +0000

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - prints it's name followed by a new line
+ * @argc - number of arguements
+ * @argv - array of arguements
+ * Return - string
+ */
+
+int main(argc, char *argv[])
+{
+	printf("%i\n", argv[0]);
+	return (0);
+}
