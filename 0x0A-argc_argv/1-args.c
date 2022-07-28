d285b8b985c9f6187fdacd83eaf8acb0b9ee4d01
Oswald Ojo <oswaldojo@ymail.com>
2022-07-28 01:45:22 +0000

    number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguements passed to it
+ * @argc: number of arguements passed
+ * @argv: array of arguements passed
+ * Return: integer
+ */
+int main(argc, char *argv[]__attribute__((unused)))
+{
+	printf("%i", argc);
+	return (0);
+}
