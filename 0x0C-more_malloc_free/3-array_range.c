cb0630a484d08fb72a69698bedfc93b4c24feba5
Oswald Ojo <oswaldojo@ymail.com>
2022-08-01 22:17:50 +0000

    more memory allocation functions

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * array_range - function that creates an array of integers.
+ * @min: integer
+ * @max: integer
+ * Return: array of integers
+ */
+int *array_range(int min, int max)
+{
+	int *ar;
+	int i;
+
+	if (min > max)
+		return (NULL);
+		ar = malloc(sizeof(*ar) * ((max - min) + 1));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
+	return (ar);
+}
