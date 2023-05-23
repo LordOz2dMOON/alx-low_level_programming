f25ec7621b7a74c6ec34d4ffbaead329ff2e4f82
Oswald Ojo <oswaldojo@ymail.com>
2023-05-23 01:23:06 +0000

    print to standard output with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - print string to standard output with printf
+ *Return: Return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
